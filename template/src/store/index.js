f6ee9949dc0fb70fb7b34f5f1d0bc037d5d577c7
Francisco Gerlison <frnaciscogerlison@ioasys.com.br>
2020-05-12 20:15:04 -0300

    Configure reactotron

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -1,9 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+
+import Reactotron from '@services/reactotron';
 
 import GlobalDucks from './ducks';
 
-const middlewares = applyMiddleware(thunk);
 const reducers = combineReducers({ ...GlobalDucks });
+
+const middlewares = compose(
+  applyMiddleware(thunk),
+  __DEV__ ? Reactotron.createEnhancer() : () => {},
+);
 
 export default createStore(reducers, middlewares);
